68ba654a0f2fa014328a84bdd57d4eb531f885c0
sofie-rk <sofierk@stud.ntnu.no>
2021-06-20 11:19:11 +0200

    many improvements :)

--- addBooks.js
+++ addBooks.js
@@ -1,42 +1,7 @@
 
-//import {books} from "./books.js"
-
 // Access components from frontPage.html
 const content = document.getElementById("content")
 
-// Each book is a JSON-object
-
-const bookTypes = ["Audiobook", "Physical Book"]
-
-const genres = ["Biography/Memoir"]
-
-
-const murderInLittleEgypt = {
-    title: "Murder in Little Egypt",
-    type: bookTypes[0],
-    length: "13h, 12min",
-    author: "Darcy O'Brien",
-    read: "May-June 2021",
-    genre: genres[0],
-    review: "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son in. \
-            Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick.\
-            Murder in Little Egyot is a story of manipulation, hatred, coldness and inhumanity. In the beginning, the reader gets a thourghough introduction to Dr. Cavaness life, long before he murdered his son.\
-            I would recommend this book for thos interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.\
-            ",
-}
-
-const aRipInHeaven = {
-    title: "A Rip in Heaven",
-    type: bookTypes[0],
-    length: "Unknown",
-    author: "Jeanine Cummings",
-    read: "March-April 2021",
-    genre: genres[0],
-    review: "True Crime. About the murder of Julie and another girl, and the assualt on the guy. Goes into the storyline and aftermath of the crime.",
-}
-
-
-const books = [murderInLittleEgypt, aRipInHeaven]
 
 
 for (let i=0; i<books.length; i++) {
@@ -44,6 +9,14 @@ for (let i=0; i<books.length; i++) {
     // Division about the book
     var bookDivision = document.createElement("div")
     bookDivision.setAttribute('class', 'bookDivision')
+
+    // Create left part
+    var leftDivision = document.createElement("div")
+    leftDivision.setAttribute('class', 'leftDivision')
+
+    // Create right part
+    var rightDivision = document.createElement("div")
+    rightDivision.setAttribute('class', 'rightDivision')
 
     // Book title
     var bookTitle = document.createElement("p")
@@ -54,6 +27,12 @@ for (let i=0; i<books.length; i++) {
     var bookAuthor = document.createElement("p")
     bookAuthor.setAttribute('class', 'bookInfo')
     bookAuthor.appendChild(document.createTextNode("Author: " + books[i].author))
+
+    // Picture
+    var bookImage = document.createElement('img')
+    bookImage.setAttribute('src', books[i].imageloc)
+    bookImage.setAttribute('alt', 'Book image')
+    bookImage.setAttribute('class', 'bookImage')
 
     // Type 
     var bookType = document.createElement("p")
@@ -81,14 +60,17 @@ for (let i=0; i<books.length; i++) {
     bookReview.appendChild(document.createTextNode(books[i].review))
 
     
+    rightDivision.appendChild(bookTitle)
+    rightDivision.appendChild(bookAuthor)
+    rightDivision.appendChild(bookReview)
 
-    bookDivision.appendChild(bookTitle)
-    bookDivision.appendChild(bookAuthor)
-    bookDivision.appendChild(bookType)
-    bookDivision.appendChild(bookLength)
-    bookDivision.appendChild(bookGenre)
-    bookDivision.appendChild(readWhen)
-    bookDivision.appendChild(bookReview)
+    leftDivision.appendChild(bookImage)
+    leftDivision.appendChild(bookType)
+    leftDivision.appendChild(bookLength)
+    leftDivision.appendChild(readWhen)
+
+    bookDivision.appendChild(leftDivision)
+    bookDivision.appendChild(rightDivision)
     
     
 
--- books.js
+++ books.js
@@ -1,3 +1,7 @@
+const bookTypes = ["Audiobook", "Physical Book"]
+
+const genres = ["Biography/Memoir"]
+
 const murderInLittleEgypt = {
     title: "Murder in Little Egypt",
     type: bookTypes[0],
@@ -5,11 +9,12 @@ const murderInLittleEgypt = {
     author: "Darcy O'Brien",
     read: "May-June 2021",
     genre: genres[0],
-    review: "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son in. \
+    review: "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son. \
             Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick.\
             Murder in Little Egyot is a story of manipulation, hatred, coldness and inhumanity. In the beginning, the reader gets a thourghough introduction to Dr. Cavaness life, long before he murdered his son.\
             I would recommend this book for thos interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.\
             ",
+    imageloc: "images/murderInLittleEgypt.jpg"
 }
 
 const aRipInHeaven = {
@@ -20,10 +25,25 @@ const aRipInHeaven = {
     read: "March-April 2021",
     genre: genres[0],
     review: "True Crime. About the murder of Julie and another girl, and the assualt on the guy. Goes into the storyline and aftermath of the crime.",
+    imageloc: "images/aRipInHeaven.jpg",
 }
 
 
 const books = [murderInLittleEgypt, aRipInHeaven]
+
+
+
+// data = '[{"name" : "Ashwin", "age" : "20"},{"name" : "Abhinandan", "age" : "20"}]';
+
+// murderInLittleEgypt = '[{
+//     "title": "Murder in Little Egypt",
+//     "type": "Audiobook",
+//     "length": "13h, 12min",
+//     "author": "Darcy O'Brien",
+//     "read": "June 2021",
+//     "genre": "Biography/Memoir",
+//     "review": "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son in. Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick. Murder in Little Egyot is a story of manipulation, hatred, coldness and inhumanity. In the beginning, the reader gets a thourghough introduction to Dr. Cavaness life, long before he murdered his son. I would recommend this book for thos interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.",
+// }]';
 
 
 
