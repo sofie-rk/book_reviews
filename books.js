0ca7b8a1df4bc1726d4f443ade66b9348a0277ba
sofie-rk <sofierk@stud.ntnu.no>
2021-06-20 12:10:08 +0200

    added review about a rip in heaven

--- books.js
+++ books.js
@@ -9,23 +9,28 @@ const murderInLittleEgypt = {
     author: "Darcy O'Brien",
     read: "May-June 2021",
     genre: genres[0],
+    imageloc: "images/murderInLittleEgypt.jpg",
     review: "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son. \
             Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick.\
             Murder in Little Egyot is a story of manipulation, hatred, coldness and inhumanity. In the beginning, the reader gets a thourghough introduction to Dr. Cavaness life, long before he murdered his son.\
-            I would recommend this book for thos interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.\
+            I would recommend this book for those interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.\
             ",
-    imageloc: "images/murderInLittleEgypt.jpg"
+    
 }
 
 const aRipInHeaven = {
     title: "A Rip in Heaven",
     type: bookTypes[0],
-    length: "Unknown",
-    author: "Jeanine Cummings",
+    length: "10h, 18min",
+    author: "Jeanine Cummins",
     read: "March-April 2021",
     genre: genres[0],
-    review: "True Crime. About the murder of Julie and another girl, and the assualt on the guy. Goes into the storyline and aftermath of the crime.",
     imageloc: "images/aRipInHeaven.jpg",
+    review: "A Rip in Heaven is Jeanine Cummins' story of a horrifying night when her cousins Julie and Robin Kerry and her brother Tom Cummins were brutally assualted. \
+            April 4th 1991, Tom, Julie and Robin went for a late-night walk on the Old Chain of Rocks Bridge outside of St. Louis, Missouri. \
+            On the bridge, they met Marlin Gray, Antonio Richardson, Reginald Clemsons and Daniel Winfrey, who robbed, assaulted and pushed them from the bridge. \
+            A Rip in Heaven is a memoir of a shocking crime and its aftermath. It's impeccably written, and the author does an excellent job of respectfully telling a story that needed to be told. \
+            I must say that I truly enjoyed reading the book, and would recommend it to anyone who likes true crime and memoirs.",
 }
 
 
