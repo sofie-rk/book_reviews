9e4a9b4f507684945a4e2d78bfcd31cde617ebfe
sofie-rk <sofierk@stud.ntnu.no>
2021-06-20 18:55:08 +0200

    added general description about some of the books

--- books.js
+++ books.js
@@ -1,6 +1,6 @@
 const bookTypes = ["Audiobook", "Physical Book"]
 
-const genres = ["Biography/Memoir"]
+const genres = ["Biography/Memoir", "Personal Success", "Comedy"]
 
 const murderInLittleEgypt = {
     title: "Murder in Little Egypt",
@@ -8,7 +8,7 @@ const murderInLittleEgypt = {
     length: "13h, 12min",
     author: "Darcy O'Brien",
     read: "May-June 2021",
-    genre: genres[0],
+    genre: [genres[0]],
     imageloc: "images/murderInLittleEgypt.jpg",
     review: "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son. \
             Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick.\
@@ -24,7 +24,7 @@ const aRipInHeaven = {
     length: "10h, 18min",
     author: "Jeanine Cummins",
     read: "March-April 2021",
-    genre: genres[0],
+    genre: [genres[0]],
     imageloc: "images/aRipInHeaven.jpg",
     review: "A Rip in Heaven is Jeanine Cummins' story of a horrifying night when her cousins Julie and Robin Kerry and her brother Tom Cummins were brutally assualted. \
             April 4th 1991, Tom, Julie and Robin went for a late-night walk on the Old Chain of Rocks Bridge outside of St. Louis, Missouri. \
@@ -33,22 +33,79 @@ const aRipInHeaven = {
             I must say that I truly enjoyed reading the book, and would recommend it to anyone who likes true crime and memoirs.",
 }
 
+const nextLevelBasic = {
+    title: "Next Level Basic - The Definitive Basic Bitch Handbook",
+    type: bookTypes[0],
+    length: "4h, 36min",
+    author: "Stassi Schroeder",
+    read: "April-May 2021",
+    genre: [genres[0], genres[1]],
+    imageloc: "images/nextLevelBasic.jpg",
+    review: "",
+}
 
-const books = [murderInLittleEgypt, aRipInHeaven]
+const giveThemLala = {
+    title: "Give them Lala",
+    type: bookTypes[0],
+    length: "5h, 44min",
+    author: "Lala Kent",
+    read: "May 2021",
+    genre: [genres[0], genres[1]],
+    imageloc: "images/giveThemLala.jpg",
+    review: "",
+}
+
+const bornACrime = {
+    title: "Born a Crime - Stories from a South African Childhood",
+    type: bookTypes[0],
+    length: "8h, 44min",
+    author: "Trevor Noah",
+    read: "April 2021",
+    genre: [genres[1], genres[2]],
+    imageloc: "images/bornACrime.jpg",
+    review: "",
+}
+
+const fiveSecondRule = {
+    title: "The 5 Second Rule - Transform your Life, Work and Confidence with Everyday Courage",
+    type: bookTypes[0],
+    length: "7h, 35min",
+    author: "Mel Robbins",
+    read: "April 2021",
+    genre: [genres[1]],
+    imageloc: "images/fiveSecondRule.jpg",
+    review: "",
+}
+
+const girlStopApologizing = {
+    title: "Girl, Stop Apologizing - A Shame-Free Plan for Embracing and Achieving Your Goals",
+    type: bookTypes[0],
+    length: "8h, 10min",
+    author: "Rachel Hollis",
+    read: "January 2021",
+    genre: [genres[1]],
+    imageloc: "images/girlStopApologizing.jpg",
+    review: "",
+}
+
+const theTruthsWeHold = {
+    title: "The Truths We Hold - An American Journey",
+    type: bookTypes[0],
+    length: "9h, 26min",
+    author: "Kamela Harris",
+    read: "December 2020-January 2021",
+    genre: [genres[0]],
+    imageloc: "images/theTruthsWeHold.jpg",
+    review: "",
+}
 
 
 
-// data = '[{"name" : "Ashwin", "age" : "20"},{"name" : "Abhinandan", "age" : "20"}]';
+const books = [murderInLittleEgypt, giveThemLala, nextLevelBasic, bornACrime, fiveSecondRule, aRipInHeaven, girlStopApologizing, theTruthsWeHold]
 
-// murderInLittleEgypt = '[{
-//     "title": "Murder in Little Egypt",
-//     "type": "Audiobook",
-//     "length": "13h, 12min",
-//     "author": "Darcy O'Brien",
-//     "read": "June 2021",
-//     "genre": "Biography/Memoir",
-//     "review": "Murder in Little Egypt is a true crime, telling the story of the unimaginable crime of a father murdering his son in. Dr. Cavaness was a well respected doctor living in a small, rural place called Eldorado in Southern Illinois. Despite him being a greedy, drunken, and selfish man, the townpeople of Eldorado worshiped him. He had 4 sons, Mark, Kevin, Sean and Patrick. Murder in Little Egyot is a story of manipulation, hatred, coldness and inhumanity. In the beginning, the reader gets a thourghough introduction to Dr. Cavaness life, long before he murdered his son. I would recommend this book for thos interested in true crime. The book is written on a detective and interpersonal level, without too many gory details.",
-// }]';
+
+
+
 
 
 
