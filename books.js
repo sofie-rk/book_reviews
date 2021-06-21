690645b8bed91becc44ab0b3904a2b58badf12b5
sofie-rk <sofierk@stud.ntnu.no>
2021-06-21 21:37:50 +0200

    added review of next level basic

--- books.js
+++ books.js
@@ -41,7 +41,17 @@ const nextLevelBasic = {
     read: "April-May 2021",
     genre: [genres[0], genres[1]],
     imageloc: "images/nextLevelBasic.jpg",
-    review: "",
+    review: "I was not a big Stassi fan after her first appereances on Vanderpump Rules. Although I grew to like her eventually, I did not expect to enjoy her book as much as I did. \
+            As the book title implies, this book is about embracing being basic a.f. \
+            A basic bitch loves all things Starbucks, selfies, social media, hot dogs, bubbly coctails, #OOTD, millenial pink... The list goes on and on. Basically, she covers every imaginable basic subject. \
+            Stassi is bright, witty and authenic. It is easy entertainment, and I must shamefully admit that I enjoyed each and every part of it. \
+            What I love the most about the book, though, is how empowering it is. When Stassi tells you to be basic a.f. she means that you should embrace being yourself. \
+            Find your own style, learn from your mistakes, and find the people that love and support you. \
+            \"Think of times you have felt your happiest, and make a list of them. And then take some time to think about why you were happy. \
+            Yes, sometimes it will be because you were with loved ones or it was your 21st birthday or your son was born. \
+            But a lot of them will be simply because you were yourself. And you would not having to pretend to be a foodie who despises hotdogs or a TV hater who would never watch Kardashians, even though you watch it on the reg. \
+            Look to those moments and aspire to keep those moments going. And that's what makes you an enlightened next level basic bitch badass.\"\
+            ",
 }
 
 const giveThemLala = {
